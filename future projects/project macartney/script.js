9ffb21e019dda474c594c4f40c44626254b4aa69
Benjamin Greenwood <80186793+LandGrab1@users.noreply.github.com>
2021-05-25 11:41:20 +1000

    stuff

--- future projects/project macartney/script.js
+++ future projects/project macartney/script.js
@@ -7,3 +7,9 @@ function click() {
     console.log(coin);
     document.getElementById("coins").innerHTML = `${coin}coins`;
 }
+
+let weight100;
+function forage() {
+    weight100 = Math.random * 100;
+    
+}
