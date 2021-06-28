1521e88d6511e351b8078ecf317c2f93a603d16a
Benjamin Greenwood <80186793+LandGrab1@users.noreply.github.com>
2021-06-29 01:51:30 +1000

    fixed engine

--- DGT/tetris/script.js
+++ DGT/tetris/script.js
@@ -1,7 +1,6 @@
-var tempstate1 = [];
-var tempstate2 = [];
+
 var currentstate = [
-    {0:"a",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
+    {0:"r",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
     {0:"n",1:"b",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
     {0:"n",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
     {0:"n",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
@@ -21,15 +20,47 @@ var currentstate = [
     {0:"n",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
     {0:"n",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
     {0:"n",1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n",},
-]
+];
 
-function fall(){
-    tempstate1 = currentstate[1];
-    currentstate[1] = currentstate[0];
-    tempstate2 = currentstate[2];
-    currentstate[2] = tempstate1;
-    tempstate1 =  currentstate[3];
-    currentstate[3] = tempstate2
-    // currentstate[1][4] = "A";
-    console.log(currentstate[2]);
+function load(){
+    document.getElementById("grid").innerHTML = "";
+    for (let i1 = 0; i1 < 20; i1++) {
+        for (let i2 = 0; i2 < 10; i2++) {
+        document.getElementById("grid").innerHTML += `<div class="${currentstate[i1][i2]}"></div>`;
+        }
+    }
+    i1 = 0;
+    i2 = 0;
+    console.log(currentstate);
 }
+
+let tt2 = 2;
+let tt1 = 1;
+var tempstate1 = [];
+let tempstate2 = currentstate[0];
+function fall(){
+    console.log(currentstate);
+    for (let tt1 = 1; tt1 < 19;) {
+        console.log(tt1, tt2);
+        tempstate1 = currentstate[tt1];
+        currentstate[tt1] = tempstate2;
+        tempstate2 = currentstate[tt2];
+        currentstate[tt2] = tempstate1;
+        tt1+=2;
+        tt2+=2;
+    }
+    tt2 = 2;
+    tt1 = 1;
+    // tempstate1 = currentstate[1];
+    // currentstate[1] = currentstate[0];
+    // tempstate2 = currentstate[2];
+    // currentstate[2] = tempstate1;
+    // tempstate1 =  currentstate[3];
+    // currentstate[3] = tempstate2
+    // currentstate[1][4] = "A";
+    console.log(currentstate);
+    load();
+}
+window.onload = function() {
+    load();
+};
